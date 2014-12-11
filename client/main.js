f1c7503ce0108087f0d7cdca386bca0344e5ba77
Illya Nizyev <illya.nizyev@samposoftware.com>
2014-12-12 00:19:14 +0200

    fixed  autoupdate marker bug

--- client/main.js
+++ client/main.js
@@ -4,28 +4,25 @@ Template.map.rendered = function () {
     // create the tile layer with correct attribution
     var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
     var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
-
+    markers = new L.LayerGroup().addTo(map);
     L.tileLayer(osmUrl, {
         attribution: osmAttrib,
         maxZoom: 18
     }).addTo(map);
-    // add a marker in the given location, attach some popup content to it and open the popup
-    L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup. <br> Easily customizable.')
-    .openPopup();
 
     map.locate({setView: true, maxZoom: 16, watch: true});
 
     function onLocationFound(e) {
+        markers.clearLayers();
         var radius = e.accuracy / 2;
-
-        L.marker(e.latlng).addTo(map)
-        .bindPopup("You are within " + radius + " meters from this point").openPopup();
-
-        L.circle(e.latlng, radius).addTo(map);
+        var marker = L.marker(e.latlng).addTo(markers);
+        var circle = L.circle(e.latlng, radius).addTo(markers);
     }
 
     map.on('locationfound', onLocationFound);
+
+
+
 }
 
 
