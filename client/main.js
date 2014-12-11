0fdc25cd00dd78bef38c449cfc56ad7286e7354f
Illya Nizyev <illya.nizyev@samposoftware.com>
2014-12-11 22:54:48 +0200

    geolocate user

--- client/main.js
+++ client/main.js
@@ -1,17 +1,31 @@
-if (Meteor.isClient) {
-    Template.map.rendered = function () {
-        // create a map in the "map" div, set the view to a given place and zoom
-        var map = L.map('map').setView([51.505, -0.09], 13);
+L.Icon.Default.imagePath = 'images';
+Template.map.rendered = function () {
+    var map = L.map('map');
+    // create the tile layer with correct attribution
+    var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
 
-        // add an OpenStreetMap tile layer
-        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
+    L.tileLayer(osmUrl, {
+        attribution: osmAttrib,
+        maxZoom: 18
+    }).addTo(map);
+    // add a marker in the given location, attach some popup content to it and open the popup
+    L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS3 popup. <br> Easily customizable.')
+    .openPopup();
 
-        // add a marker in the given location, attach some popup content to it and open the popup
-        L.marker([51.5, -0.09]).addTo(map)
-        .bindPopup('A pretty CSS3 popup. <br> Easily customizable.')
-        .openPopup();
+    map.locate({setView: true, maxZoom: 16});
+
+    function onLocationFound(e) {
+        var radius = e.accuracy / 2;
+
+        L.marker(e.latlng).addTo(map)
+        .bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+        L.circle(e.latlng, radius).addTo(map);
     }
 
+    map.on('locationfound', onLocationFound);
 }
+
+
